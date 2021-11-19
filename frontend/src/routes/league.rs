3036e0287e227dec6b7259812067c72a3fc0cffc
JtotheThree <johnmullins@gmail.com>
2021-11-18 19:36:13 -0600

    Before updating yew

--- frontend/src/routes/league.rs
+++ frontend/src/routes/league.rs
@@ -32,20 +32,9 @@ impl Component for League {
         html! {
             <div class="px-4">
             <div
-              class="
-                flex
-                justify-center
-                items-center
-                mx-auto
-                max-w-2xl
-                my-16
-                p-16
-                border-2
-                border-black
-                font-raleway
-              "
+              class="flex items-center justify-center max-w-2xl p-16 mx-auto my-16 border-2 border-black  font-raleway"
             >
-               <h1>{ self.props.league_id.clone() }</h1>
+            <h1>{ self.props.league_id.clone() }</h1>
             </div>
             </div>
         }
---
+++ frontend_test/src/main.rs
@@ -0,0 +1,7 @@
+use sycamore::prelude::*;
+
+fn main() {
+    sycamore::render(|| view! {
+        p { "Hello, World! " }
+    });
+}
